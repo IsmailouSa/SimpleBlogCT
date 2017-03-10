4b5363f9f993b610fb75e9cced10ab0cfde6a954
PC20818\L20805 <ismailou.sa@gmail.com>
2017-03-10 10:50:06 +0200

    Form and auth namespace (model binding) added

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,11 @@ namespace SimpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            return Content("Hi " + form.Username + ", your password is : " + form.Password);
         }
     }
 }
