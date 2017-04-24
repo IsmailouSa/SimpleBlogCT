3d692a8ffed98777d1730dba3ab3fcb551b68eeb
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-04-25 01:29:34 +0300

    Style bundle added

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles (BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/Admin.css")
+                );
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/Site.css")
+                );
+        }
+    }
+}
