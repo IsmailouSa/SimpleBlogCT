48f24d8b00f4f3a9865457cbddb7e96a2e44444f
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-05-06 23:46:54 +0300

    Database created using FluentMigrator

---
+++ SimpleBlog/Migrations/_001_Users_and_Roles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Migrations
+{
+    public class _001_Users_and_Roles
+    {
+    }
+}
