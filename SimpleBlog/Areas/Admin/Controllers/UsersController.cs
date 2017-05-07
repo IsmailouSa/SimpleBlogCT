6bcb5ef554a64891aec20bfa8219afbe37657b65
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-05-07 20:06:35 +0300

    User entity added

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.Areas.Admin.ViewModels;
+using SimpleBlog.Models;
+using NHibernate.Linq;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
@@ -14,7 +17,8 @@ namespace SimpleBlog.Areas.Admin.Controllers
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return View();
+            var users = Database.Session.Query<User>().ToList(); //select * from users
+            return View(new UsersIndex() { Users = users });
         }
 
         public ActionResult List()
